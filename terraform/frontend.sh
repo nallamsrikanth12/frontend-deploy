c68a6618a010ce24c7deaae769b641ebfdef3d93
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-07-16 08:06:59 +0530

    modified

--- terraform/frontend.sh
+++ terraform/frontend.sh
@@ -4,4 +4,4 @@ environment=$2
 appversion=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/nallamsrikanth12/expense-ansible-roles.git main.yaml -e component=$component -e env=$environment appversion=$appversion
+ansible-pull -i localhost, -U https://github.com/nallamsrikanth12/expense-ansible-roles.git main.yaml -e component=$component -e env=$environment -e appversion=$appversion
